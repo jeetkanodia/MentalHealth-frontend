d8dde4f8e7537698e79f1f8b2971116d47112289
jeet kanodia <jeetkanodia@gmail.com>
2023-11-08 18:25:29 +0530

    Add footer with links and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,59 @@
 import React from "react";
-
+import "../styles/footer.css";
+import logo from "../assets/happy-test.png";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="footer-logo">
+          <img src={logo} alt="Mental Health Logo" />
+        </div>
+        <div class="footer-links">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div class="footer-social">
+            <ul>
+              <li>
+                <a href="#">
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-info">
+        <p>&copy; 2023 Mental Health Website</p>
+        <p>Privacy Policy | Terms of Use</p>
+      </div>
+    </footer>
+  );
 };
-
 export default Footer;
