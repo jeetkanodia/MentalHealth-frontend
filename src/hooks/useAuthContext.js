e459082ad2aec2579a43bc75b188298d28d89c0d
jeet kanodia <jeetkanodia@gmail.com>
2023-11-15 16:25:13 +0530

    Add AuthContextProvider to App.jsx

---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,14 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuthContext must be used within an AuthContextProvider"
+    );
+  }
+
+  return context;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
