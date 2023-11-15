435ae232b8af7c7ed2d4ec56950cfc224f2a8e61
jeet kanodia <jeetkanodia@gmail.com>
2023-11-15 22:43:17 +0530

    Add useEffect hook to retrieve user from localStorage on mount

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -22,6 +22,15 @@ export const AuthContextProvider = ({ children }) => {
     user: null,
   });
 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({
+        type: "LOGIN",
+        payload: user,
+      });
+    }
+  }, []);
   console.log("AuthContext state: ", state);
 
   return (
