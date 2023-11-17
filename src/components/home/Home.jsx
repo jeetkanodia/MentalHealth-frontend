6b1c62be27c2f6b3de0632d7fa659c8d81f94690
jeet kanodia <jeetkanodia@gmail.com>
2023-11-17 19:51:35 +0530

    Refactor home component and styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,58 +1,54 @@
 import React from "react";
-import image1 from '../../assets/mind-plan.png';
-import '../../styles/home.css';
-import { styled, Box, Typography } from '@mui/material';
-import background from '../../assets/home-mental.jpg';
-
+import image1 from "../../assets/mind-plan.png";
+import "../../styles/home.css";
+import { styled, Box } from "@mui/material";
+import background from "../../assets/home-mental.jpg";
+import { Link } from "react-router-dom";
 
 const Image = styled(Box)`
-    width: 100%;
-    background: url(${background}) center/82%;
-    height: 60vh;
-    display: flex;
-    flex-direction: column;
-   
-    justify-content: center;
+  width: 100%;
+  background: url(${background}) center/82%;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
-
-
-
-
 
 const Home = () => {
   return (
-    <div className="home">
+    <div style={{ marginTop: "15vh" }} className="home">
       <main>
         <Image>
-        <section className="mental-health-intro">
-          <h1 className="mental-health-intro-title">
-            Every Mind Matters</h1>
-
-          <article>
-            <h2>What is mental health?</h2>
-            <p>
-              Having good mental health helps us relax more, achieve more and enjoy our lives more. 
-              There are simple things we can all do to look after our mental health and wellbeing – 
-              take our quiz to get started today with a free plan, expert advice and practical tips.
-            </p>
-          </article>
-        </section>
+          <section className="mental-health-intro">
+            <h1 className="mental-health-intro-title">Every Mind Matters</h1>
+            <article>
+              <h2>What is mental health?</h2>
+              <p>
+                Having good mental health helps us relax more, achieve more and
+                enjoy our lives more. There are simple things we can all do to
+                look after our mental health and wellbeing – take our quiz to
+                get started today with a free plan, expert advice and practical
+                tips.
+              </p>
+            </article>
+          </section>
         </Image>
         <section className="mind-plan">
-          <h2>Do something now...</h2>
           <div className="mind-plan-content">
             <div className="mind-plan-info">
               <h3>Get Your Mind Plan</h3>
               <p>
-                Take our quiz to get your free plan, designed to help you feel more in control, 
-                deal with stress and anxiety, boost your mood and improve your sleep.
+                Take our carefully crafted quiz to discover your mental health
+                score on things like depression, happiness scale, workplace
+                stress etc; and get personalised tips and advice on what you can
+                do to look after your mental health.
               </p>
-              <button onClick={() => {/* handle click event to take the quiz */}}>
-                Take the quiz
-              </button>
+              <Link to="/test">
+                <button>Take the quiz</button>
+              </Link>
             </div>
             <div className="mind-plan-image">
-                <img src={image1} alt="Mind Plan" />
+              <img src={image1} alt="Mind Plan" />
             </div>
           </div>
         </section>
